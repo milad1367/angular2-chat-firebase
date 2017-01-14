9cf19cd78d9ca04ae80d22ee30291e988c092bc6
miladas01 <miladasghari2106@gmail.com>
2017-01-14 16:07:02 +0330

    add chat list component

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class ChatComponent {
+  roomId: string;
+  constructor(private route: ActivatedRoute) {
+    this.roomId = this.route.snapshot.params['roomId'];
   }
-
+ 
 }
