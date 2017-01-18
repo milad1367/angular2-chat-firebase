9b18cb131c67af9f6351bdec90fb6602edb30a52
miladas01 <miladasghari2106@gmail.com>
2017-01-18 17:09:29 +0330

    add chat message form component

--- src/app/chat-message-form/chat-message-form.component.ts
+++ src/app/chat-message-form/chat-message-form.component.ts
@@ -1,5 +1,7 @@
 import {Component, Inject,Input} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { ChatMessagesService } from '../chat-messages.service';
+import { ChatMessage } from '../chat-message';
 class ChatMessageFormModel {
   author: string;
   body: string;
@@ -11,15 +13,21 @@ class ChatMessageFormModel {
 })
 export class ChatMessageFormComponent {
   @Input() roomId: string;
-    private chatMessageForm: FormGroup;
-
-  form: FormGroup;
-  constructor(private fb: FormBuilder) {
+  private chatMessageForm: FormGroup;
+  constructor(private fb: FormBuilder,private cs: ChatMessagesService) {
     this.chatMessageForm = this.fb.group({
       'author': ['', Validators.required],
       'body': ['', Validators.required]
     });
   }
 
+    saveMessage() {
+    if (this.chatMessageForm.valid) {
+      this.cs.createNewMessage(this.roomId, <ChatMessage> this.chatMessageForm.value);
+      const author = this.chatMessageForm.controls['author'].value;
+      this.chatMessageForm.reset();
+      this.chatMessageForm.controls['author'].setValue(author);
+    }
+  }
 
 }
